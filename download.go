6609ed110445a054d522124db05fbeb2f9b91597
Tad Hunt <tadhunt@gmail.com>
2024-03-23 22:11:02 -0600

    First public release

---
+++ download.go
@@ -0,0 +1,73 @@
+package httptools
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/tadhunt/retry"
+	"github.com/tadhunt/logger"
+)
+
+const (
+	DL_InitialDelay = 500 * time.Millisecond
+	DL_MaxDelay     = 2 * time.Second
+	DL_MaxTries     = 5
+)
+
+func HTTPDownload(srcUrl string, dstPath string) error {
+	log := logger.NewCompatLogWriter(logger.LogLevel_DEBUG)
+
+	f, err := os.Create(dstPath)
+	if err != nil {
+		return log.ErrFmt("%s: %v", srcUrl, err)
+	}
+
+	defer f.Close()
+
+	retrier := retry.NewRetrier(DL_MaxTries, DL_InitialDelay, DL_MaxDelay)
+
+	start := time.Now()
+	last := start
+	attempt := 0
+
+	ctx := context.Background()
+
+	err = retrier.RunContext(ctx, func(c context.Context) error {
+		now := time.Now()
+		total := now.Sub(start)
+		sinceLast := now.Sub(last)
+		last = now
+
+		attempt++
+
+		log.Debugf("%s: attempt %d (elapsed %s, since last %s)", srcUrl, attempt, total, sinceLast)
+
+		r, err := http.Get(srcUrl)
+		if err != nil {
+			return retry.Stop(log.ErrFmt("%s: %v", srcUrl, err))
+		}
+		defer r.Body.Close()
+
+		switch {
+		default:
+			return retry.Stop(log.ErrFmt("%s: %v", srcUrl, r.Status))
+		case r.StatusCode == http.StatusOK:
+			_, err = io.Copy(f, r.Body)
+			if err != nil {
+				return retry.Stop(log.ErrFmt("%s: %v", srcUrl, err))
+			}
+			return nil
+		case r.StatusCode >= 500:
+			return log.ErrFmt("retry")
+		}
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/tadhunt/httptools
+
+go 1.22.1
+
+require (
+	github.com/tadhunt/logger v0.0.0-20240319184922-7a0408f863ee
+	github.com/tadhunt/retry v0.0.0-20201126032642-04f0ec3feb8d
+)
+
+require (
+	golang.org/x/mod v0.16.0 // indirect
+	golang.org/x/tools v0.19.0 // indirect
+)
